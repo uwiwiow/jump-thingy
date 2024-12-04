24a29ca571060f08ff73654d216eaa579f6d1119
uwiwiow <uwiwiow@gmail.com>
2024-12-03 19:33:46 -0600

    platform respawn

--- src/main.c
+++ src/main.c
@@ -4,6 +4,10 @@
 
 void drawPlayerOutOfScreen(Vector2 pos, Vector2 player, Color color);
 void velocityVerlet(Vector2 *position, Vector2 *velocity, float mass, float timestep);
+void generatePlatforms(Vector2 platforms[5], Vector2 platformSize, float *start, float gap);
+void drawPlatforms(Vector2 platforms[5], Vector2 platformSize);
+void checkCollisionPlatforms(Vector2 platforms[5], Vector2 platformSize, Rectangle playerPointColl, Rectangle killZone, Vector2 *velocity, float *start, float gap);
+void drawPlatformsPosScreen(Vector2 platforms[5]);
 
 int main(void) {
 
@@ -13,7 +17,6 @@ int main(void) {
     // player
     Vector2 player = {30, 40};
     Vector2 pos = {(float) WIDTH / 2, 800};
-    Vector2 screenPos;
     Vector2 velocity = Vector2Zero();
     Rectangle playerPointColl;
     float mass = 1.0f;
@@ -28,7 +31,14 @@ int main(void) {
     // game
     float timestep = 0.0f;
     Rectangle ground = {0, 800, WIDTH, HEIGHT};
-    Rectangle platform = {center.x, 550, 80, 10};
+
+    Vector2 platforms [5];
+    Vector2 platformSize = {80, 10};
+    float platformStart = 550.0f;
+    float platformGap = 250.0f;
+    Rectangle killZone = {0.0f, HEIGHT, WIDTH, 200.0f};
+
+    generatePlatforms(platforms, platformSize, &platformStart, platformGap);
 
     while(!WindowShouldClose()) {
 
@@ -51,21 +61,24 @@ int main(void) {
         playerPointColl = (Rectangle) {pos.x, pos.y + player.y - 1, player.x, 1};
         if (CheckCollisionRecs(playerPointColl, ground))
             velocity.y = jumpSpd;
-        if (CheckCollisionRecs(playerPointColl, platform) && velocity.y > 0.0f)
-            velocity.y = jumpSpd;
+
+        checkCollisionPlatforms(platforms, platformSize, playerPointColl, killZone, &velocity, &platformStart, platformGap);
 
         // set camera target
         target.y = target.y - 200.0f > pos.y ? pos.y + 200.0f : target.y;
         camera.target = target;
 
-        // get player screen pos and check game over
-        screenPos = GetWorldToScreen2D(pos, camera);
-        if (screenPos.y > 900.0f) {
+        // update kill zone and check game over
+        killZone.y = target.y + center.y;
+        if (CheckCollisionPointRec(pos, killZone)) {
             velocity = Vector2Zero();
             pos = (Vector2) {(float) WIDTH / 2, 800};
             target = center;
-        }
 
+            platformStart = 550.0f;
+            platformGap = 250.0f;
+            generatePlatforms(platforms, platformSize, &platformStart, platformGap);
+        }
 
         BeginDrawing();
 
@@ -75,7 +88,7 @@ int main(void) {
 
                 // draw game scene
                 DrawRectangleRec(ground, DARKGREEN);
-                DrawRectangleRec(platform, PURPLE);
+                drawPlatforms(platforms, platformSize);
 
                 // draw player
                 DrawRectangleV(pos, player, RED);
@@ -83,7 +96,8 @@ int main(void) {
 
             EndMode2D();
 
-            DrawText(TextFormat("POS\n%.2f\n%.2f\n\nVEL\n%.2f\n%.2f\n\nSCREEN POS\n%.2f\n%.2f", pos.x, pos.y, velocity.x, velocity.y, screenPos.x, screenPos.y), 10, 10, 20, WHITE);
+            DrawText(TextFormat("POS\n%.2f\n%.2f\n\nVEL\n%.2f\n%.2f", pos.x, pos.y, velocity.x, velocity.y), 10, 10, 20, WHITE);
+            drawPlatformsPosScreen(platforms);
             DrawFPS(10, HEIGHT - 30);
 
         EndDrawing();
@@ -117,4 +131,48 @@ void velocityVerlet(Vector2 *position, Vector2 *velocity, float mass, float time
 
     velocity->x += timestep * 0.5f * (acceleration.x + newAcceleration.x);
     velocity->y += timestep * 0.5f * (acceleration.y + newAcceleration.y);
+}
+
+void generatePlatforms(Vector2 platforms[5], Vector2 platformSize, float *start, float gap) {
+    for(int i = 0; i < 5; i++) {
+        platforms[i] = (Vector2) {(float) GetRandomValue(0, WIDTH - platformSize.x), *start};
+        *start -= gap;
+    }
+}
+
+void drawPlatforms(Vector2 platforms[5], Vector2 platformSize) {
+    for(int i = 0; i < 5; i++) {
+        DrawRectangleV(platforms[i], platformSize, PURPLE);
+    }
+}
+
+void updatePlatform(Vector2 *platform, Vector2 platformSize, float *start, float gap) {
+    *platform = (Vector2) {(float) GetRandomValue(0, WIDTH - platformSize.x), *start};
+    *start -= gap;
+}
+
+void checkCollisionPlatforms(Vector2 platforms[5], Vector2 platformSize, Rectangle playerPointColl, Rectangle killZone, Vector2 *velocity, float *start, float gap) {
+    Rectangle platform;
+    for(int i = 0; i < 5; i++) {
+        platform = (Rectangle) {platforms[i].x, platforms[i].y, platformSize.x, platformSize.y};
+        if (CheckCollisionRecs(playerPointColl,
+                               platform)
+                               && velocity->y > 0.0f) {
+            velocity->y = jumpSpd;
+        }
+        if (CheckCollisionRecs(killZone,
+                               platform)) {
+            updatePlatform(&platforms[i], platformSize, start, gap);
+        }
+
+    }
+}
+
+void drawPlatformsPosScreen(Vector2 platforms[5]) {
+    int start = 185;
+    int gap = 16;
+    for(int i = 0; i < 5; i++) {
+        DrawText(TextFormat("%d\t\t%.0f\t\t%.0f", i, platforms[i].x, platforms[i].y), 10, start, 20, WHITE);
+        start += gap;
+    }
 }
